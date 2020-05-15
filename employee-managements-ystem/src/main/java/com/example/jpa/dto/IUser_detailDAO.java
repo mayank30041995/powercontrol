900c3da9180312b79cfc164e00b09724d9a6010b
mayank <mayankece155@gmail.com>
2020-05-15 20:27:52 +0530

    Rest-api project using hibernate

--- employee-managements-ystem/src/main/java/com/example/jpa/dto/IUser_detailDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.jpa.dto;
-
-
-public interface IUser_detailDAO {
-
-
-
-}
--- testapplication/src/testapplication/Test.java
+++
@@ -1,8 +0,0 @@
-package testapplication;
-
-public class Test {
-
-	public static void main(String [] args) {
-		System.out.println("hello World");
-	}
-}
